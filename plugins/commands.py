d3b196ea232db30a0b1e98c94a0f5045498f0b7a
Gokulbots <botsdemo9@gmail.com>
2024-11-14 20:08:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -739,7 +739,7 @@ async def set_shortner(c, m):
     try:
         URL = m.command[1]
         API = m.command[2]
-        resp = requests.get(f'https://{URL}/api?api={API}&url=https://telegram.dog/Jisshu_support').json()
+        resp = requests.get(f'https://{URL}/api?api={API}&url=https://telegram.dog/CINEMACCBOTDISCUSSION').json()
         if resp['status'] == 'success':
             SHORT_LINK = resp['shortenedUrl']
         await save_group_settings(grp_id, 'shortner', URL)
